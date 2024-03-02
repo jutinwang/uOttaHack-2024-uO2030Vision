8926f4042bfea7847c5e518a23655990d5856745
Jack Snelgrove <jacksnelgrove@Jacks-MacBook-Pro.local>
2024-03-02 09:34:47 -0500

    Added firebase

---
+++ uo2024-vision/src/views/Sign_in.vue
@@ -0,0 +1,48 @@
+<!-- Sign_in.vue -->
+<template>
+  <div>
+    <h2>Sign In</h2>
+    <form @submit.prevent="authenticate">
+      <label for="email">Email:</label>
+      <input type="email" v-model="email" id="email" required>
+
+      <label for="password">Password:</label>
+      <input type="password" v-model="password" id="password" required>
+
+      <button type="submit">Sign In</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    authenticate() {
+      console.log("Auth")
+      const email = this.email;
+      const password = this.password;
+      console.log("Logged in")
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+      
+      const user = userCredential.user;
+       // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+    }
+  }
+};
+</script>
