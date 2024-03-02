66ea654b532f6ca95ab07f94d5c7bfe07c2f321c
Eli Wynn <eli.wynn7@gmail.com>
2024-03-02 17:18:33 -0500

    Complete frontend revamp and database tweaks

--- uo2024-vision/src/App.vue
+++ uo2024-vision/src/App.vue
@@ -1,12 +1,4 @@
 <template>
-  <nav>
-  <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Services</a></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
-</nav>
   <router-view/>
 </template>
 
--- uo2024-vision/src/views/RoomTypes.vue
+++ uo2024-vision/src/views/RoomTypes.vue
@@ -1,26 +1,38 @@
-<template>
-  <ul>
-    <li>
-      <button @click="signOut">Sign Out</button>
-    </li>
-    <li v-for="room in roomTypes" :key="room.id" @click="redirectToRoomPage(room.id)">
-      <h3>{{ room.id }} {{ room.name }}</h3>
-    </li>
-  </ul>
+<template id="butt">
+  <header id="roomTypeHeader">  
+    <img alt="logo" src="../assets/logo.png" height="75">
+    <h1 id="togather">TOGATHER</h1>
+    <button id="signOutBtn" @click="signOut">Sign Out</button>
+  </header>
+  <body id="roomTypeBody">
+    <div class="roomTypes">
+      <ul class="roomGrid">
+        <li v-for="(room) in roomTypes" :key="room.id" @click="redirectToRoomPage(room.id)">
+          <h3>{{ room.id }} - {{ room.name }}</h3>
+        </li>
+      </ul>
+    </div>
+  </body>
 </template>
 
 <script>
-import axios from 'axios';
 import { getAuth, signOut } from "firebase/auth";
-
 
 export default {
   name: 'RoomTypeList',
   data() {
     return {
-      roomTypes:[
-        {id: 1, name: 'Library'},
-        {id: 2, name: 'Exams'},
+      roomTypes: [
+        { id: 1, name: 'Library' },
+        { id: 2, name: 'Exams' },
+        { id: 3, name: 'Conference Room' },
+        { id: 4, name: 'Study Room' },
+        { id: 5, name: 'Meeting Room' },
+        { id: 6, name: 'Game Room' },
+        { id: 7, name: 'Lounge' },
+        { id: 8, name: 'Auditorium' },
+        { id: 9, name: 'Training Room' },
+        { id: 10, name: 'Break Room' }
       ]
     };
   },
@@ -47,7 +59,67 @@ export default {
 </script>
 
 <style>
-li{
-  display: inline-block
+h1#togather {
+  color: #6c0e23;
+  text-shadow: -1px -1px 0 #7C93C3, 1px -1px 0 #7C93C3, -1px 1px 0 #7C93C3, 1px 1px 0 #7C93C3; /* Outline using shadows */
+  font-size: 36px;
+  margin: 0;
 }
+
+header#roomTypeHeader {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #A25772;
+  height: 100px;
+  padding: 0 20px; /* Adjust padding */
+  box-sizing: border-box; /* Ensure padding is included in width */
+}
+
+img[alt="logo"] {
+  height: 100%;
+}
+
+button#signOutBtn {
+  height: 50px;
+  width: 100px;
+  border-radius: 25px;
+  background-color: #6c0e23;
+  color: white;
+  border: none;
+  font-size: 16px;
+}
+
+body#roomTypeBody {
+  margin: 0;
+  padding-top: 3%;
+  height: 100vh;
+  background-color: #EEF5FF;
+  padding-left: 2%;
+  padding-right: 2%;
+}
+
+.roomTypes ul.roomGrid {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 20px;
+  padding: 0;
+  justify-items: center; /* Center items horizontally */
+  list-style-type: none;
+}
+
+.roomTypes ul li {
+  cursor: pointer;
+}
+
+.roomTypes ul li h3 {
+  color: #6c0e23;
+  font-size: 18px;
+  text-align: center;
+  margin: 0;
+  padding: 10px;
+  border: 1px solid #A25772;
+  border-radius: 10px;
+}
+
 </style>
