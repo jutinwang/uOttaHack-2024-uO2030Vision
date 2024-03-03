da8d23fd4faed39638e05fb9f8b7f5e5fca9a17a
Eli Wynn <eli.wynn7@gmail.com>
2024-03-03 01:40:02 -0500

    large changes to RoomsView

--- uo2024-vision/src/views/RoomsView.vue
+++ uo2024-vision/src/views/RoomsView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Remove the button -->
-    <div v-for="room in rooms" :key="room.ID" @click="redirectToRoom(room.ID)">
+    <div v-for="room in rooms" :key="room.ID" @click="redirectToRoom(room.ID)" :class="[getClassByRoomType()]">
       <h3>{{ room.attendents ? room.attendents.length : 0 }} / 30</h3>
     </div>
   </div>
@@ -15,7 +15,8 @@ export default {
   name: 'RoomList',
   data() {
     return {
-      rooms: []
+      rooms: [],
+      
     };
   },
   methods: {
@@ -44,6 +45,35 @@ export default {
       });
       // Or using window.location.href:
       //window.location.href = `/room/${roomID}`; // Assuming the route for room details is '/room/:id'
+    },
+    getClassByRoomType() {
+      const type = this.$route.params.id;
+      console.log(type);
+      // Define class based on room type
+      switch (type) {
+        case "1":
+          return 'isRoom1'; // Apply isRoom1 class for roomType 1
+        case "2":
+          return 'isRoom2'; // Apply isRoom2 class for roomType 2
+        case "3":
+          return 'isRoom3';
+        case "4":
+          return 'isRoom4';
+        case "5":
+          return 'isRoom5';
+        case "6":
+          return 'isRoom6';
+        case "7":
+          return 'isRoom7';
+        case "8":
+          return 'isRoom8';
+        case "9":
+          return 'isRoom9';
+        case "10":
+          return 'isRoom10';
+        default:
+          return 'isFart'; // Default class if no specific styling needed
+      }
     }
   },
   mounted() {
@@ -58,11 +88,74 @@ h3 {
   display: inline-block;
   margin: 0 10px;
   padding: 10px;
-  border-radius: 20%;
-  background-color: #9EB8D9;  
   border: 2px solid #7C93C3;
   height: 170px;
   width: 170px;
   cursor: pointer; /* Add cursor pointer to indicate clickable */
+  margin-top: 5%;
+}
+
+
+.isRoom1{
+  height: 100vh;  
+  background-image: url('../assets/1722.jpg');
+  background-size: cover; /* Apply tint */
+  background-position: center;
+  h3{
+    background-image: url('../assets/book.png');
+  }
+}
+
+.isRoom2{
+  height: 100vh;  
+  background-image: url('../assets/desks.jpg');
+  background-size: cover; /* Apply tint */  
+  background-position: center;
+  h3{
+    background-image: url('../assets/exambook.png');
+  }
+}
+
+.isRoom3{
+  height: 100vh;  
+  background-image: url('../assets/code.jpg');
+  background-size: cover; /* Apply tint */  
+  background-position: center;
+  h3{
+    background-image: url('../assets/computers.png');
+    background-position: center;
+    background-size: cover;
+    width: 190px;
+  }
+}
+
+.isRoom4{
+  height: 100vh;  
+  background-image: url('../assets/schematic.jpg');
+  background-size: cover; /* Apply tint */  
+  background-position: center;
+  
+  h3{
+    background-image: url('../assets/calculator.png');
+    background-position: center;
+    height: 200px;
+    border-radius: 5%;
+  }
+}  
+
+.isRoom5{
+  height: 100vh;  
+  background-image: url('../assets/tropical-green-leaves-background.jpg');
+  background-size: cover; /* Apply tint */  
+  background-position: center;
+  
+  h3{
+    background-image: url('../assets/penguin.png');
+    background-position: center;
+    border-radius: 10%;
+  }
+}  
+.isFart{
+  background-color: blue;
 }
 </style>
